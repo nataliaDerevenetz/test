01702627787f2178e15066f0ff8629334fe217af
Natalia Derevenetz <nna911@inbox.ru>
2025-10-12 16:46:41 +0300

    branch test 3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         //some test
         //some branch
         //2
-
+        //3
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
